e04d3f9915490364744cbb336db31183e8b4f2d0
AN-7878 <akshita.nagurkar@gmail.com>
2025-08-03 18:51:14 +0530

    errors on parent dashbaoard and drawer fixed

--- lib/parent_dashboard/pdashboard.dart
+++ lib/parent_dashboard/pdashboard.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:vaccine_scheduler/parent_dashboard/announcement_list.dart';
 import 'appointment_page/appointment.dart';
 import 'package:vaccine_scheduler/parent_dashboard/child_profile/child_profile.dart';
-import 'package:vaccine_scheduler/login_page/login.dart';
+//import 'package:vaccine_scheduler/login_page/login.dart';
+import 'package:vaccine_scheduler/parent_dashboard/moreaboutvaccines.dart';
 import 'package:vaccine_scheduler/parent_dashboard/vaccine_timeline/timeline.dart';
 import 'package:vaccine_scheduler/parent_dashboard/home_landing/home_landing.dart';
-import 'package:vaccine_scheduler/parent_dashboard/widgets/back_to_home_wrapper.dart';
+import 'package:vaccine_scheduler/parent_dashboard/faqs.dart';
+import 'package:vaccine_scheduler/parent_dashboard/parent_drawer.dart';
 
 class ParentDashboard extends StatefulWidget {
   final String email;
@@ -17,103 +20,34 @@ class ParentDashboard extends StatefulWidget {
 class _ParentDashboardState extends State<ParentDashboard> {
   int _selectedIndex = 0;
 
-  final List<Widget> _pages = [];
+  late final List<Widget> _pages;
 
-@override
-void initState() {
-  super.initState();
-  _pages.addAll([
-    ParentHomeLanding(email: widget.email), // index 0
-
-    BackToHomeWrapper(
-      onBack: () {
-        setState(() => _selectedIndex = 0);
-      },
-      child: AppointmentScreen(email: widget.email),
-    ),
-
-    BackToHomeWrapper(
-      onBack: () {
-        setState(() => _selectedIndex = 0);
-      },
-      child: ChildProfilePage(),
-    ),
-
-    BackToHomeWrapper(
-      onBack: () {
-        setState(() => _selectedIndex = 0);
-      },
-      child: VaccineTimeline(),
-    ),
-  ]);
-}
-
+  @override
+  void initState() {
+    super.initState();
+    _pages = [
+      ParentHomeLanding(email: widget.email),
+      AppointmentScreen(email: widget.email),
+      ChildProfilePage(),
+      VaccineTimeline(),
+      FAQs(),
+      MoreAboutVaccines(),
+      AnnouncementListScreen(),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Parent Dashboard")),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(color: Colors.blue),
-              child: Text(
-                "Welcome, ${widget.email}",
-                style: const TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {
-                setState(() => _selectedIndex = 0);
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.calendar_month),
-              title: const Text('Appointments'),
-              onTap: () {
-                setState(() => _selectedIndex = 1);
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.child_care),
-              title: const Text('Child Profile'),
-              onTap: () {
-                setState(() => _selectedIndex = 2);
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.timeline),
-              title: const Text('Vaccine Timeline'),
-              onTap: () {
-                setState(() => _selectedIndex = 3);
-                Navigator.pop(context);
-              },
-            ),
-            const Divider(),
-            ListTile(
-              leading: const Icon(Icons.logout),
-              title: const Text('Logout'),
-              onTap: () {
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (_) => const LoginPage()),
-                  (route) => false,
-                );
-              },
-            ),
-          ],
-        ),
+      drawer: ParentDrawer(
+        email: widget.email,
+        onItemSelected: (index) {
+          setState(() => _selectedIndex = index);
+          Navigator.pop(context);
+        },
       ),
       body: _pages[_selectedIndex],
     );
   }
 }
-
-
